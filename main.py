03f43819d3a625277bd2f439708ddbf6f44a84c8
Jade Herd <toxicaven@disroot.org>
2024-01-09 13:21:09 -0500

    Use buffers instead of temp files, add checkreg command

--- main.py
+++ main.py
@@ -1,119 +1,102 @@
-import discord
-from dotenv import load_dotenv
-import os
-import sys
 from cleaninty.ctr.simpledevice import SimpleCtrDevice
+from cleaninty.ctr.soap.manager import CtrSoapManager
 from pyctr.type.exefs import ExeFSReader
-import shutil
+from cleaninty.ctr.soap import helpers
+from io import BytesIO, StringIO
+from dotenv import load_dotenv
+import discord
+import json
+import os
 
 bot = discord.Bot()
 load_dotenv()
-os.mkdir("/tmp/soop")
 
 @bot.slash_command(description="Generate a consoles soap key")
 async def genjson(ctx: discord.ApplicationContext,
                     secinfo: discord.Option(discord.Attachment, "secinfo.bin, SecureInfo_A"),
                     otp: discord.Option(discord.Attachment, "otp.bin"), 
                     country: discord.Option(str, "Required for U and E regions", required=False)):
-    
-    try:
-        tmpdir = f"/tmp/soop/{ctx.interaction.id}"
-        os.mkdir(tmpdir)
-    except:
-        await ctx.respond(ephemeral=True, content="Failed to create interaction workspace")
-        return
+    await ctx.defer(ephemeral=True)
 
     try:
-        await otp.save(f"{tmpdir}/otp.bin")
-        await secinfo.save(f"{tmpdir}/secinfo.bin")
-    except:
-        await ctx.respond(ephemeral=True, content="Failed to save uploaded content")
-        shutil.rmtree(tmpdir)
-        return
-    
-    try:
-        SimpleCtrDevice.generate_new_json(
-            otp_file=f"{tmpdir}/otp.bin",
-            secureinfo_file=f"{tmpdir}/secinfo.bin",
+        soapJson = SimpleCtrDevice.generate_new_json(
+            otp_data=await otp.read(),
+            secureinfo_data=await secinfo.read(),
             country=country,
-            json_file=f"{tmpdir}/soap.json"
         )
     except Exception as e:
-        await ctx.respond(ephemeral=True, content=f"Cleaninty error: {e}")
-        shutil.rmtree(tmpdir)
+        await ctx.respond(ephemeral=True, content=f"Cleaninty error:\n```\n{e}\n```")
         print(f"Cleaninty: {e}")
         return
     
     try:
-        await ctx.respond(ephemeral=True, file=discord.File(f"{tmpdir}/soap.json"))
-    except Exception as e:
-        print("Failed to respond, what")
-        print(e.with_traceback())
+        await ctx.respond(ephemeral=True, file=discord.File(fp=StringIO(soapJson), filename="soap.json"))
+    except:
+        await ctx.respond(ephemeral=True, content="Failed to respond with soap.json")
 
-    shutil.rmtree(tmpdir)
 
 @bot.slash_command(description="Generate a consoles soap key using essential.exefs")
 async def genjsonessential(ctx: discord.ApplicationContext,
                            essential: discord.Option(discord.Attachment, "essential.exefs"),
                            country: discord.Option(str, "Required for U and E regions", required=False)):
-    try:
-        tmpdir = f"/tmp/soop/{ctx.interaction.id}"
-        os.mkdir(tmpdir)
-    except:
-        await ctx.respond(ephemeral=True, content="Failed to create interaction workspace")
-        return
+    await ctx.defer(ephemeral=True)
     
     try:
-        await essential.save(f"{tmpdir}/essential.exefs")
-    except:
-        await ctx.respond(ephemeral=True, content="Failed to save uploaded content")
-        shutil.rmtree(tmpdir)
-        return
-    
-    try:
-        reader = ExeFSReader(f"{tmpdir}/essential.exefs")
+        reader = ExeFSReader(BytesIO(await essential.read()))
     except:
         await ctx.respond(ephemeral=True, content="Failed to read essential")
-        shutil.rmtree(tmpdir)
         return
     
     if not "secinfo" and "otp" in reader.entries:
         await ctx.respond(ephemeral=True, content="Invalid essential")
-        shutil.rmtree(tmpdir)
         return
+
     try:
-        with open(f"{tmpdir}/otp.bin", "wb") as f:
-            f.write(reader.open("otp").read())
-            f.close()
-        
-        with open(f"{tmpdir}/secinfo.bin", "wb") as f:
-            f.write(reader.open("secinfo").read())
-            f.close()
-            reader.close()
-    except:
-        await ctx.respond(ephemeral=True, content="Failed to copy out of essential.exefs")
-        shutil.rmtree(tmpdir)
-        return
-    
-    try:
-        SimpleCtrDevice.generate_new_json(
-            otp_file=f"{tmpdir}/otp.bin",
-            secureinfo_file=f"{tmpdir}/secinfo.bin",
-            country=country,
-            json_file=f"{tmpdir}/soap.json"
+        soapJson = SimpleCtrDevice.generate_new_json(
+            otp_data=reader.open("otp").read(),
+            secureinfo_data=reader.open("secinfo").read(),
+            country=country
         )
     except Exception as e:
         await ctx.respond(ephemeral=True, content=f"Cleaninty error:\n```\n{e}\n```")
-        shutil.rmtree(tmpdir)
         print(f"Cleaninty: {e}")
         return
     
     try:
-        await ctx.respond(ephemeral=True, file=discord.File(f"{tmpdir}/soap.json"))
+        await ctx.respond(ephemeral=True, file=discord.File(fp=StringIO(soapJson), filename="soap.json"))
+    except:
+        await ctx.respond(ephemeral=True, content="Failed to respond with soap.json")
+
+@bot.slash_command(description="check console registry")
+async def checkreg(ctx : discord.ApplicationContext, jsonfile: discord.Option(discord.Attachment, "soap.json")):
+    await ctx.defer(ephemeral=True)
+
+    try:
+        jsonStr = await jsonfile.read()
+        jsonStr = jsonStr.decode("utf-8")
+        json.loads(jsonStr) # Validate the json, output useless
+    except:
+        await ctx.respond(ephemeral = True, content="Failed to load json")
+        return
+
+    try:
+        dev = SimpleCtrDevice(json_string=jsonStr)
+        soapMan = CtrSoapManager(dev, False)
+        helpers.CtrSoapCheckRegister(soapMan)
+
+        retStr = ""
+        retStr += f"Account status: {soapMan.account_status}\n"
+        if soapMan.account_status != 'U':
+            retStr += f"Account register: {'Expired' if soapMan.register_expired else 'Valid'}\n"
+        retStr += f"Current effective region: {soapMan.region}\n"
+        retStr += f"Current effective country: {soapMan.country}\n"
+        retStr += f"Current effective language: {soapMan.language}\n"
     except Exception as e:
-        print("Failed to respond, what")
-        print(e.with_traceback())
-    shutil.rmtree(tmpdir)
+        await ctx.respond(ephemeral=True, content=f"Cleaninty error:\n```\n{e}\n```")
+        return
+    
+    await ctx.respond(ephemeral=True, content=f"```\n{retStr}```")
+
 
 
 @bot.event
